f9aecf735382b302e78a5da62323e6e291454f9a
blakegandy <gandyb98@gmail.com>
2020-09-22 11:09:54 -0400

    adds associations to actor and movie models.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,8 +4,5 @@ class Actor < ApplicationRecord
   validates :known_for, presence: true
   validates :age, numericality: {greater_than: 13}
 
-
-  Actor.create(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
-  
-  Actor.create(first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock")
+  belongs_to :movie
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :director, presence: true
   validates :year, numericality: {only_integer: true}
   validates :plot, length: {maximum: 500}
+
+  has_many :actors
 end
