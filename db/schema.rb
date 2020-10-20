edc20c9a07e8cda4ca6d3d27907b2c0d9bd94b1e
blakegandy <gandyb98@gmail.com>
2020-10-20 11:20:05 -0400

    adds users and login capabilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_153452) do
+ActiveRecord::Schema.define(version: 2020_10_20_145557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,14 @@ ActiveRecord::Schema.define(version: 2020_09_25_153452) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "director"
     t.boolean "english", default: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
